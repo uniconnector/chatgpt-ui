6e1266da8e2d80b69c4b9ce5779e2d01fefdf77f
13404302982 <rui.zhang@uniconnector.com>
2023-11-23 20:07:05 +0800

    Solve the problem of previewing HTML code for markdown

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './assets/material-design-iconic-font.min.css'
 import './assets/style.min.css'
 
-// vue-markdown import
+// preview html
+// import VMdPreviewHtml from '@kangc/v-md-editor/lib/preview-html'
+// import '@kangc/v-md-editor/lib/style/preview-html.css'
+// import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+
+// preview vue-markdown 
 import VMdPreview from '@kangc/v-md-editor/lib/preview'
 import '@kangc/v-md-editor/lib/style/preview.css'
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
@@ -12,6 +17,11 @@ import hljs from 'highlight.js';
 
 VMdPreview.use(githubTheme, {
     Hljs: hljs,
+    extend(md) {
+        md.set({
+          html: false
+        });
+    },
 })
 
 import { createApp } from 'vue'
@@ -24,5 +34,7 @@ const app = createApp(App)
 app.use(router)
 
 app.use(VMdPreview)
+
+// app.use(VMdPreviewHtml)
 
 app.mount('#app')
